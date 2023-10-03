73a102d237b015403adc6546bd7e9b11c67ba328
Pharisaeuss <146725724+Pharisaeuss@users.noreply.github.com>
2023-10-03 17:28:03 +0300

    Create Class
    Created class attributes and class constructor

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Game;
+
+public class GameAccount 
+{
+    public string UserName;
+    public int CurrentRating;
+    public int GamesCount;
+
+    public GameAccount(string username, int currentrating, int gamescount)
+    {
+        UserName = username;
+        CurrentRating = currentrating;
+        GamesCount = gamescount;
+    }
+
+    
+}
